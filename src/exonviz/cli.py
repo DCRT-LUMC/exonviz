0bd212dc45bb2659b7a8ba64aa2f8a0282844cfc
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2023-06-20 16:02:13 +0200

    Begin implementation draw_exon

---
+++ src/exonviz/cli.py
@@ -0,0 +1,18 @@
+"""
+Module that contains the command line app, so we can still import __main__
+without executing side effects
+"""
+
+import argparse
+from exonviz.exon import draw_exon
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Description of command.")
+    args = parser.parse_args()
+    plot = draw_exon([21, 23, 21])#, 21, 23])
+    print(plot)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/exonviz/exon.py
@@ -0,0 +1,52 @@
+from typing import List
+import svg
+
+
+def draw_exon(exons: List[int], scale:int = 10) -> svg.SVG:
+    elements = list()
+
+    # Default positions
+    y_position = 10
+    height = 10
+    exon_gap = 5
+
+    # This x position will be updated
+    x_position = 10
+    start_frame = 0
+
+    for exon in exons:
+
+        end_frame = (start_frame + exon) %3
+
+        if start_frame == 0 and end_frame == 0:
+            points = [
+                x_position, y_position, #  topleft
+                x_position+exon, y_position, #  topright
+                x_position+exon, y_position + height, #  bottom right
+                x_position, y_position + height  # bottomleft
+            ]
+        elif start_frame == 0 and end_frame == 2:
+            points = [ 
+                x_position, y_position,
+                x_position+exon, y_position,
+                x_position+exon-height, y_position + 0.5*height,
+                x_position+exon, y_position+height,
+                x_position, y_position + height,
+            ]
+        else:
+            import sys
+            print(f"start_frame = {start_frame}, end_frame = {end_frame}", file=sys.stderr)
+            continue
+            raise NotImplementedError()
+
+        x_position = x_position + exon + exon_gap
+
+        # Scale the points
+        points = [x*scale for x in points]
+
+        elements.append(svg.Polygon(points = points, stroke='green', stroke_width=1))
+
+        start_frame = end_frame
+
+    return svg.SVG(width=1000, height=700, elements=elements)
+            
