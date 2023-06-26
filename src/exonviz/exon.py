e410b1e95208f6bc09dd7ff8f12cd84c8aaf0310
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2023-06-26 14:45:43 +0200

    Fix formatting

--- src/exonviz/exon.py
+++ src/exonviz/exon.py
@@ -2,9 +2,11 @@ from typing import List, Union
 import svg
 
 
-def shift(points: List[Union[float,int]], x_offset: int, y_offset:int) -> List[Union[float,int]]:
-    """ Shift the x- and y position by the supplied offsets"""
-    return [x+y_offset if i%2 else x+x_offset for i,x in enumerate(points)]
+def shift(
+    points: List[Union[float, int]], x_offset: int, y_offset: int
+) -> List[Union[float, int]]:
+    """Shift the x- and y position by the supplied offsets"""
+    return [x + y_offset if i % 2 else x + x_offset for i, x in enumerate(points)]
 
 
 def draw_exons(exons: List[int], scale: int = 1, canvas_width: int = 1000) -> svg.SVG:
@@ -30,13 +32,13 @@ def draw_exons(exons: List[int], scale: int = 1, canvas_width: int = 1000) -> sv
         # Shift the points
         points = shift(points, x_position, y_position)
         # Scale the points
-        points = [x*scale for x in points]
+        points = [x * scale for x in points]
 
         x_position = x_position + exon + exon_gap
         end_frame = (start_frame + exon) % 3
 
         fill = "green" if start_frame == end_frame else "black"
-        elements.append(svg.Polygon(points = points, stroke='green', fill=fill, stroke_width=1)) # type: ignore
+        elements.append(svg.Polygon(points=points, stroke="green", fill=fill, stroke_width=1))  # type: ignore
 
         start_frame = end_frame
 
@@ -44,7 +46,7 @@ def draw_exons(exons: List[int], scale: int = 1, canvas_width: int = 1000) -> sv
 
 
 def draw_exon(height: int, exon: int, start_frame: int) -> List[float]:
-    """ Draw the specified exon by calculating all the x,y points that have to be connectd
+    """Draw the specified exon by calculating all the x,y points that have to be connectd
 
     We draw clockwise, starting from the top left
 
@@ -59,7 +61,7 @@ def draw_exon(height: int, exon: int, start_frame: int) -> List[float]:
 
     # Pre-calculate the ends for the 6 possible frames
     # Use the frame to fetch the index
-    #fmt: off
+    # fmt: off
     left_end = [
         tuple(), #  Straigt line, we don't need to draw anything
         (0.5*height, 0.5*height), #  point
@@ -72,7 +74,7 @@ def draw_exon(height: int, exon: int, start_frame: int) -> List[float]:
         (exon-0.5*height, 0.5*height), #  notch
 
     ]
-    #fmt: on
+    # fmt: on
 
     return [
         *top_left,
@@ -81,5 +83,5 @@ def draw_exon(height: int, exon: int, start_frame: int) -> List[float]:
         *bottom_right,
         *bottom_left,
         *left_end[start_frame],
-        *top_left
+        *top_left,
     ]
