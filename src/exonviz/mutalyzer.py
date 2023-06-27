e3d588cbde5ba0452577f6f6b1caee60e2c1875a
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2023-06-27 10:20:16 +0200

    Add some code to interact with mutalyzer web API

---
+++ src/exonviz/mutalyzer.py
@@ -0,0 +1,29 @@
+from typing import Dict, Any, Optional
+import urllib.request
+import json
+
+from exonviz.exon import Exon
+
+def mutalyzer(variant: str) -> Optional[Dict[str, Any]]:
+    """Fetch variant information from mutalyzer"""
+
+    url=f"https://mutalyzer.nl/api/normalize/{variant}"
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            js = json.loads(response.read())
+    except:
+        print(f"Error for {url}")
+        return None
+    return js["selector_short"]
+
+
+def cds_start(mutalyzer: Dict[str, Any]) -> int:
+    """Extract the coding start from mutalyzer payload"""
+    return int(mutalyzer["cds"]["g"][0][0])-1
+
+
+def cds_end(mutalyzer: Dict[str, Any]) -> int:
+    """Extract the coding start from mutalyzer payload"""
+    return int(mutalyzer["cds"]["g"][0][1])
+
---
+++ tests/test_mutalyzer.py
@@ -0,0 +1,34 @@
+from exonviz.mutalyzer import cds_start, cds_end
+
+# Example mutalyzer payload
+mutalyzer = {
+    "exon" : {
+        "g": [
+            ["1", "268"],
+            ["269", "330"],
+            ["11284", "13992"]
+        ],
+    },
+    "cds": {
+        "g": [
+            ["238", "11295"]
+        ]
+    }
+}
+
+def test_cds_start() -> None:
+    assert cds_start(mutalyzer) == 237
+
+
+def test_cds_end() -> None:
+    assert cds_end(mutalyzer) == 11295
+
+
+def test_get_coding_size_noncoding():
+    """Test the size for non-coding exons"""
+    exons = [
+        ["1", "100"],
+        ["201", "300"]
+    ]
+    coding_start = 200
+
