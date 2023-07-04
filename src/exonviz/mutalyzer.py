331e807bcb78702df89642fa85829e7869b44b73
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2023-07-04 17:45:44 +0200

    Reverse positions if transcript is on the reverse strand

--- src/exonviz/mutalyzer.py
+++ src/exonviz/mutalyzer.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Optional, List
+from typing import Dict, Any, Optional, List, Tuple
 import urllib.request
 from urllib.error import HTTPError
 import json
@@ -22,33 +22,41 @@ def mutalyzer(variant: str) -> Optional[Dict[str, Any]]:
     return selector
 
 
-def cds_start(mutalyzer: Dict[str, Any]) -> int:
-    """Extract the coding start from mutalyzer payload"""
-    return int(mutalyzer["cds"]["g"][0][0]) - 1
+def convert_coding_positions(positions: List[List[str]]) -> Tuple[int, int]:
+    start = int(positions[0][0])
+    end = int(positions[0][1])
 
+    if start > end:
+        start, end = end, start
 
-def cds_end(mutalyzer: Dict[str, Any]) -> int:
-    """Extract the coding start from mutalyzer payload"""
-    return int(mutalyzer["cds"]["g"][0][1])
+    start = start - 1
+    return (start, end)
 
+def convert_exon_positions(positions : List[List[str]]) -> List[Tuple[int, int]]:
+    converted = list()
+    for mutalyzer_start, mutalyzer_end in positions:
+        start = int(mutalyzer_start)
+        end = int(mutalyzer_end)
 
-def coding_region(mutalyzer: Dict[str, Any]) -> Region:
-    return Region(cds_start(mutalyzer), cds_end(mutalyzer))
+        # Make sure start is always before end
+        if start > end:
+            start, end = end, start
 
+        start = start - 1
+        converted.append((start, end))
+    return converted
 
 def extract_exons(mutalyzer: Dict[str, Any]) -> List[Exon]:
     """Extract Exons from mutalyzer payload"""
     exons: List[Exon] = list()
 
     # Get the coding region
-    coding = Region(cds_start(mutalyzer), cds_end(mutalyzer))
+    coding = Region(*convert_coding_positions(mutalyzer["cds"]["g"]))
 
     # The first exon starts in frame by definition
     frame = 0
 
-    for start, end in mutalyzer["exon"]["g"]:
-        start = int(start) - 1
-        end = int(end)
+    for start, end in convert_exon_positions(mutalyzer["exon"]["g"]):
         E = Exon(start, end, frame, coding)
         frame = E.end_frame
         exons.append(E)
--- tests/test_mutalyzer.py
+++ tests/test_mutalyzer.py
@@ -1,4 +1,8 @@
-from exonviz.mutalyzer import cds_start, cds_end, coding_region, extract_exons
+from exonviz.mutalyzer import (
+    extract_exons,
+    convert_exon_positions,
+    convert_coding_positions,
+)
 from exonviz.exon import Exon, Region
 
 # Example mutalyzer payload
@@ -10,18 +14,51 @@ mutalyzer = {
 }
 
 
-def test_cds_start() -> None:
-    assert cds_start(mutalyzer) == 237
+def test_convert_coding_positions() -> None:
+    assert convert_coding_positions([["238", "11295"]]) == (237, 11295)
 
 
-def test_cds_end() -> None:
-    assert cds_end(mutalyzer) == 11295
-
-
-def test_coding_region() -> None:
-    assert coding_region(mutalyzer) == Region(237, 11295)
+def test_convert_coding_positions_reverse() -> None:
+    assert convert_coding_positions([["29199", "7218"]]) == (7217, 29199)
 
 
 def test_extract_exons() -> None:
     exons = extract_exons(mutalyzer)
     assert exons[0] == Exon(0, 268, 0, Region(237, 11295))
+
+
+def test_convert_mutalyzer_positions() -> None:
+    positions = [["1", "268"], ["269", "330"], ["11284", "13992"]]
+
+    assert convert_exon_positions(positions) == [
+        (0, 268),
+        (268, 330),
+        (11283, 13992),
+    ]
+
+
+def test_convert_mutalyzer_positions_reverse() -> None:
+    # Taken from ENST00000436367.6
+    positions = [
+        ["462349", "462187"],
+        ["454236", "454122"],
+        ["358790", "358665"],
+        ["286796", "286669"],
+        ["223577", "223516"],
+        ["186998", "186861"],
+        ["29359", "27283"],
+        ["7748", "1"],
+    ]
+
+    expected = [
+        (462186, 462349),
+        (454121, 454236),
+        (358664, 358790),
+        (286668, 286796),
+        (223515, 223577),
+        (186860, 186998),
+        (27282, 29359),
+        (0, 7748),
+    ]
+
+    assert convert_exon_positions(positions) == expected
