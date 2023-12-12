02807226e50744eff100944939654919828fd9f8
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2023-12-12 10:25:21 +0100

    Create Coding region from mutalyzer payload

---
+++ src/exonviz/mutalyzer2.py
@@ -0,0 +1,107 @@
+from typing import Dict, Any, Optional, List, Tuple
+import urllib.request
+from urllib.error import HTTPError
+import json
+
+from .exon2 import Exon, Coding
+from GTGT.range import intersect
+
+Range = Tuple[int, int]
+
+
+def mutalyzer(variant: str) -> Optional[Dict[str, Any]]:
+    """Fetch variant information from mutalyzer"""
+
+    url = f"https://mutalyzer.nl/api/normalize/{variant}"
+
+    try:
+        response = urllib.request.urlopen(url)
+    except HTTPError as e:
+        msg = f"Fetching '{url}' returned {e}"
+        raise RuntimeError(msg)
+    else:
+        js = json.loads(response.read())
+
+    if "selector_short" not in js:
+        msg = f"No exons found for {variant} (is it a genomic variant?)"
+        raise RuntimeError(msg)
+    selector: Dict[str, Any] = js["selector_short"]
+    return selector
+
+def convert_coding_positions(positions: List[List[str]]) -> Range:
+    start = int(positions[0][0])
+    end = int(positions[0][1])
+
+    if start > end:
+        start, end = end, start
+
+    start = start - 1
+    return (start, end)
+
+
+def is_reverse(positions: List[List[str]]) -> bool:
+    """Is the transcript in reverse"""
+    start = int(positions[0][0])
+    end = int(positions[0][1])
+
+    return start > end
+
+
+def convert_exon_positions(positions: List[List[str]]) -> List[Tuple[int, int]]:
+    """Convert exon positions from Mutalyzer to a list of Ranges
+
+    This function also accounts for reverse transcripts
+    """
+    converted = list()
+    
+    reversed = is_reverse(positions)
+    for mutalyzer_start, mutalyzer_end in positions:
+        start = int(mutalyzer_start)
+        end = int(mutalyzer_end)
+
+        # Make sure start is always before end
+        if start > end:
+            start, end = end, start
+
+        start = start - 1
+        converted.append((start, end))
+    if reversed:
+        return converted[::-1]
+    else:
+        return converted
+
+def make_coding(exon: Range, coding_region: Range, start_phase: int) -> Coding:
+    """Create the coding region"""
+    c = intersect(exon, coding_region)
+    if not c:
+        return Coding()
+
+    assert len(c) == 1
+    start, end = c[0]
+
+    end_phase = (start_phase + (end - start))%3
+    return Coding(
+        start=start,
+        end = end,
+        start_phase = start_phase,
+        end_phase = end_phase
+    )
+
+def extract_exons(mutalyzer: Dict[str, Any]) -> List[Exon]:
+    """Extract Exons from mutalyzer payload"""
+    exons: List[Exon] = list()
+
+    # Get the coding region
+    coding_region = convert_coding_positions(mutalyzer["cds"]["g"])
+
+    # The first exon starts in frame by definition
+    start_phase = 0
+
+    for range in convert_exon_positions(mutalyzer["exon"]["g"]):
+        coding = make_coding(range, coding_region, start_phase)
+        start_phase = coding.end_phase
+        #E = Exon(range[0], range[1], frame, coding)
+        #frame = E.end_frame
+        #exons.append(E)
+
+    return [Exon(100)]
