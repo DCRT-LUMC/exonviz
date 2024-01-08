bb7b20ecd66d34c7b45618c9d1e8c5955af44493
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2024-01-08 14:08:59 +0100

    Enable support for python 3.12

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
         # 'Programming Language :: Python :: Implementation :: Jython',
